d4cb0c90cae715c85a324d0c076e273ef88d16b4
Bruno Wosiak <brunofwosiak@gmail.com>
2020-01-30 22:59:28 -0300

    Definição de layout inicial
    fonte: https://startbootstrap.com/previews/sb-admin/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideMenuComponent,
+    FooterComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-content',
+  templateUrl: './main-content.component.html'
+})
+export class MainContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
