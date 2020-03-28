24e1133faca2809eae4aeac46b5ca7898638db43
Bruno Wosiak <brunofwosiak@gmail.com>
2020-03-28 18:11:53 -0300

    update angular version to 9

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { HomeComponent } from './home/home.component'
 export const ROUTES: Routes = [
     {path : 'home', component: HomeComponent},
     {path : '', pathMatch: 'full', redirectTo: '/home'},
-    {path : 'movimentacoes', loadChildren: './meses/meses.module#MesesModule'},
-    {path : 'categorias', loadChildren: './categorias/categorias.module#CategoriasModule'},
-    {path : 'controles', loadChildren: './controles/controles.module#ControlesModule'},
-    {path : 'ano/:id' , loadChildren: './controle-anual/controle-anual.module#ControleAnualModule'}
+    {path : 'movimentacoes', loadChildren: () => import('./meses/meses.module').then(m => m.MesesModule)},
+    {path : 'categorias', loadChildren: () => import('./categorias/categorias.module').then(m => m.CategoriasModule)},
+    {path : 'controles', loadChildren: () => import('./controles/controles.module').then(m => m.ControlesModule)},
+    {path : 'ano/:id' , loadChildren: () => import('./controle-anual/controle-anual.module').then(m => m.ControleAnualModule)}
 ]
